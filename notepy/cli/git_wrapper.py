3802b68a95040b8e44e334ec5cf634449f1228ea
Lorenzo Drumond <drumondlorenzo@posteo.net>
2023-10-26 20:29:18 +0100

    refactor, better error handling, added .gitignore

--- notepy/cli/git_wrapper.py
+++ notepy/cli/git_wrapper.py
@@ -6,9 +6,41 @@ from __future__ import annotations
 import subprocess
 from typing import Optional
 from pathlib import Path
+from notepy.cli.base_cli import BaseCli, CliException
+from shlex import split
 
 
-# TODO: add content of stdout to error messages
+def run_and_handle(command: str,
+                   cwd=".",
+                   comment="") -> subprocess.CompletedProcess:
+    """
+    Utility function for CalledProcessError easy handling. It calls a command
+    and manages exceptions by calling GitException, together with the stderr
+    of the process.
+
+    :param command: the command to execute.
+    :param cwd: the working directory of the environment for the command.
+    :param comment: optional comment to add to the exception message.
+    :return: the completed process obect.
+    """
+    split_cmd = split(command)
+    process_result = subprocess.run(split_cmd,
+                                    cwd=cwd,
+                                    stderr=subprocess.STDOUT,
+                                    stdout=subprocess.PIPE)
+
+    process_returncode = process_result.returncode
+    if process_returncode != 0:
+        error_message = (f'Command "{command}" returned a non-zero exit status '
+                         f"{process_returncode}. Below is the full stderr:\n\n"
+                         f"{process_result.stdout.decode('utf-8')}")
+        error_message = error_message + \
+            f"\n\n{comment}" if comment else error_message
+        raise GitException(error_message)
+
+    return process_result
+
+
 class Git(BaseCli):
     """
     Wrapper for git cli
@@ -19,8 +51,7 @@ class Git(BaseCli):
     def __init__(self, path: Path):
         super().__init__('git')
         self.path = path.expanduser()
-        self.git_path = self.path.joinpath(".git")
-        self._cmd_exists()
+        self.git_path = self.path / ".git"
         self._is_repo()
 
     def _is_repo(self) -> None:
@@ -28,9 +59,9 @@ class Git(BaseCli):
         Check that the directory provided is a git repo.
         """
         if not self.path.is_dir():
-            raise GitException(f"{self.path} is not a directory.")
+            raise GitException(f"'{self.path}' is not a directory.")
         if not self.git_path.is_dir():
-            raise GitException(f"{self.path} is not a git repository.")
+            raise GitException(f"'{self.path}' is not a git repository.")
 
     @classmethod
     def init(cls, path: Path) -> Git:
@@ -41,22 +72,26 @@ class Git(BaseCli):
         :return: a Git wrapper
         """
         path = path.expanduser()
-        git_path = path.joinpath(".git")
-        if not path.is_dir():
-            raise GitException(f"{path} is not a directory.")
-        if git_path.is_dir():
-            raise GitException(f"{path} is already a git repository.")
+        git_path = path / ".git"
 
-        try:
-            # TODO: add .gitignore for .index.db
-            subprocess.run(['git', 'init', path],
-                           check=True, capture_output=True)
-            subprocess.run(['git', 'add', '.'], check=True,
-                           capture_output=True, cwd=path)
-            subprocess.run(['git', 'commit', '-m', 'First commit'],
-                           capture_output=True, cwd=path, check=True)
-        except subprocess.CalledProcessError as e:
-            raise GitException(f"""Something went wrong: {e}""")
+        # sanity checks
+        if not path.is_dir():
+            raise GitException(f"'{path}' is not a directory.")
+        if git_path.is_dir():
+            raise GitException(f"'{path}' is already a git repository.")
+
+        # create gitignore
+        gitignore = path / ".gitignore"
+        gitignore.touch(exist_ok=True)
+        ignore_objects = ['.index.db', 'scratchpad', '.last']
+        with open(gitignore, "w") as f:
+            for ignored in ignore_objects:
+                f.write(ignored+"\n")
+
+        process = run_and_handle("git init", cwd=path)
+        process = run_and_handle("git add .", cwd=path)
+        process = run_and_handle("git commit -m 'First commit'", cwd=path)
+        del process
 
         new_repo = cls(path)
 
@@ -66,59 +101,53 @@ class Git(BaseCli):
         """
         Add changed files to staging area.
         """
-        try:
-            subprocess.run([self.cmd, 'add', '-A'], check=True,
-                           cwd=self.path, capture_output=True)
-        except subprocess.CalledProcessError as e:
-            raise GitException(f"""Something went wrong: {e}""")
+        process = run_and_handle("git add -A", cwd=self.path)
+        del process
 
     def commit(self, msg: Optional[str] = "commit notes") -> None:
         """
         Commit the staging area.
         """
-        try:
-            subprocess.run([self.cmd, 'commit', '-m', msg],
-                           check=True, cwd=self.path, capture_output=True)
-        except subprocess.CalledProcessError as e:
-            raise GitException(f"""Something went wrong: {e}""")
+        process = run_and_handle(f"git commit -m '{msg}'")
+        del process
 
     def push(self) -> None:
         """
         Push to origin.
         """
-        try:
-            if self._check_origin():
-                subprocess.run([self.cmd, 'push'], check=True,
-                               cwd=self.path, capture_output=True)
-            else:
-                raise GitException("""origin does not exist.""")
-        except subprocess.CalledProcessError as e:
-            raise GitException(f"""Something went wrong: {e}.
-                Check that origin is correct.""")
+        if not self._origin_exists():
+            raise GitException("""origin does not exist.""")
+
+        process = run_and_handle('git push',
+                                 cwd=self.path,
+                                 comment="Check that origin is correct")
+        del process
 
     def add_origin(self, origin: str) -> None:
         """
         Add remote origin.
         """
-        if self._check_origin():
+        if self._origin_exists():
             raise GitException("""origin already exists.""")
-        try:
-            subprocess.run([self.cmd, 'remote', 'add', 'origin',
-                           origin], check=True, cwd=self.path, capture_output=True)
-            subprocess.run([self.cmd, 'push', 'origin', 'master', '--set-upstream'],
-                           check=True, capture_output=True, cwd=self.path)
-        except subprocess.CalledProcessError as e:
-            raise GitException(f"""Something went wrong: {e}.
-                Check that origin is correct""")
 
-    def _check_origin(self) -> bool:
+        process = run_and_handle(f'git remote add origin "{origin}"',
+                                 cwd=self.path,
+                                 comment="Check that origin is correct")
+        process = run_and_handle("git push origin master --set-upstream",
+                                 cwd=self.path,
+                                 comment="Check that origin is correct")
+        del process
+
+    def _origin_exists(self) -> bool:
         """
         Check if origin is defined.
         """
-        origin = subprocess.run([self.cmd,
+        origin = subprocess.run(['git',
                                  'config',
                                  '--get',
-                                 'remote.origin.url'], cwd=self.path, capture_output=True)
+                                 'remote.origin.url'],
+                                cwd=self.path,
+                                capture_output=True)
 
         origin_exists = True
         if origin.returncode == 1:  # error code given by this failed action
@@ -133,7 +162,7 @@ class Git(BaseCli):
         return str(self.path)
 
     def __str__(self) -> str:
-        string = f"git repository at {self.path}\n\n"
+        string = f"git repository at '{self.path}'\n\n"
         string += f"{self.status}"
 
         return string
@@ -143,11 +172,9 @@ class Git(BaseCli):
         """
         Check status of current git repo.
         """
-        status = subprocess.run([self.cmd, 'status'],
-                                check=True,
-                                capture_output=True,
-                                encoding='utf-8',
-                                cwd=self.path).stdout
+        process = run_and_handle("git status",
+                                 cwd=self.path)
+        status = process.stdout.decode('utf-8')
 
         return status
 
