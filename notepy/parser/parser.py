f0a072ed425f3e4dd7e8832ca7b478cab057b91f
Lorenzo Drumond <drumondlorenzo@posteo.net>
2023-11-05 04:24:31 +0000

    turn zk_id into integer

--- notepy/parser/parser.py
+++ notepy/parser/parser.py
@@ -67,7 +67,7 @@ class HeaderParser(BaseParser):
 
     def __init__(self, parsing_obj: Collection[str],
                  delimiter: str = '---',
-                 special_names: Collection[str] = ['date', 'tags']):
+                 special_names: Collection[str] = ['date', 'tags', 'zk_id']):
         self.parsing_obj = parsing_obj
         self.delimiter = delimiter
         self.special_names = special_names
@@ -167,6 +167,16 @@ class HeaderParser(BaseParser):
                 f"There is an error in the date format: {e}")
 
         return parsed_date
+
+    def _zk_id_parser(self, zk_id: str) -> int:
+        """
+        Turn ID into integer.
+
+        :param zk_id: the ID of the note, as a string.
+        :return: the ID of the note, as an integer.
+        """
+
+        return int(zk_id)
 
     def _tags_parser(self, tags: str) -> set[str]:
         """
